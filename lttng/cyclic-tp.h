3b3e40c508ef216adb79e7fb2765f7f5dec1a4a9
Kurt Kanzenbach <kurt@kmk-computers.de>
2018-08-26 15:34:45 +0200

    examples: added lttng example
    Signed-off-by: Kurt Kanzenbach <kurt@kmk-computers.de>

---
+++ lttng/cyclic-tp.h
@@ -0,0 +1,25 @@
+#undef TRACEPOINT_PROVIDER
+#define TRACEPOINT_PROVIDER cyclic
+
+#undef TRACEPOINT_INCLUDE
+#define TRACEPOINT_INCLUDE "./cyclic-tp.h"
+
+#if !defined(_CYCLIC_TP_H) || defined(TRACEPOINT_HEADER_MULTI_READ)
+#define _CYCLIC_TP_H
+
+#include <lttng/tracepoint.h>
+
+TRACEPOINT_EVENT(
+    cyclic,
+    cyclic_tp,
+    TP_ARGS(
+        char *, string_arg
+    ),
+    TP_FIELDS(
+        ctf_string(string_field, string_arg)
+        )
+    )
+
+#endif /* _CYCLIC_TP_H_ */
+
+#include <lttng/tracepoint-event.h>
