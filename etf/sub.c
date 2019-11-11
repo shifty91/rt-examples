5f5a4a78be6c9565fb1be01aa6bd25dc3c076c6b
Kurt Kanzenbach <kurt@kmk-computers.de>
2019-11-11 20:58:08 +0100

    etf/sub: Add XDP support for subscriber
    XDP maybe useful for reducing latency and jitter on receiver side. Therefore,
    add support for it.
    
    Signed-off-by: Kurt Kanzenbach <kurt@kmk-computers.de>

--- etf/sub.c
+++ etf/sub.c
@@ -1,3 +1,11 @@
+/*
+ * Copyright (C) 2019 Kurt Kanzenbach <kurt@kmk-computers.de>
+ *
+ * XDP code inspired by
+ *  - https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/tree/samples/bpf/xdpsock_user.c?h=v5.4-rc8
+ *  - https://github.com/xdp-project/xdp-tutorial/tree/master/advanced03-AF_XDP
+ *  - https://github.com/muvarov/ipxdp
+ */
 #define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
@@ -12,6 +20,7 @@
 #include <poll.h>
 #include <limits.h>
 
+#include <net/if.h>
 #include <netdb.h>
 
 #include <sys/mman.h>
@@ -20,43 +29,99 @@
 
 #include <arpa/inet.h>
 
+#include "rt_config.h"
+
+#ifdef WITH_XDP
+#include <linux/if_link.h>
+#include <linux/if_xdp.h>
+#include <linux/if_ether.h>
+#include <linux/udp.h>
+#include <linux/ip.h>
+
+#include <bpf/bpf.h>
+#include <bpf/xsk.h>
+#include <bpf/libbpf.h>
+#endif
+
 #include "utils.h"
 
 static struct option long_options[] = {
-    { "host",      optional_argument, NULL, 'H' }, /* default: localhost */
-    { "port",      optional_argument, NULL, 'P' }, /* default: 6666 */
-    { "priority",  optional_argument, NULL, 'p' }, /* default: 99 */
-    { "cpu",       optional_argument, NULL, 'c' }, /* default: cpu 0 */
-    { "help",      no_argument,       NULL, 'h' },
+    { "host",        optional_argument, NULL, 'H' }, /* default: localhost */
+    { "port",        optional_argument, NULL, 'P' }, /* default: 6666 */
+    { "interface",   optional_argument, NULL, 'i' }, /* default: eth0 */
+    { "priority",    optional_argument, NULL, 'p' }, /* default: 99 */
+    { "cpu",         optional_argument, NULL, 'c' }, /* default: cpu 0 */
+    { "xdp",         optional_argument, NULL, 'x' }, /* default: no */
+    { "skb_mode",    optional_argument, NULL, 's' }, /* default: no */
+    { "queue",       optional_argument, NULL, 'q' }, /* default: 0 */
+    { "break_value", optional_argument, NULL, 'B' }, /* default: 0 */
+    { "base_time",   optional_argument, NULL, 'b' }, /* default: 0 */
+    { "wakeup_time", optional_argument, NULL, 'w' }, /* default: 500us */
+    { "intervall",   optional_argument, NULL, 'I' }, /* default: 0 */
+    { "help",        no_argument,       NULL, 'h' },
     { NULL },
 };
 
 /* options */
 static const char *host;
 static const char *port;
+static const char *interface;
 static int priority;
 static int cpu;
+static int xdp;
+static int skb_mode;
+static int queue;
+static long long break_value_ns;
+static long long base_time_ns;
+static long long wakeup_time_ns;
+static long long intervall_ns;
+
+/* gobal */
+static volatile int stop;
 
 /* udp */
 static int udp_socket;
 static struct sockaddr_in udp_sin;
+
+/* xdp */
+#ifdef WITH_XDP
+#define NUM_FRAMES         4096
+#define FRAME_SIZE         XSK_UMEM__DEFAULT_FRAME_SIZE
+
+unsigned int ifindex;
+struct xsk_umem_info {
+    struct xsk_ring_prod fq;
+    struct xsk_ring_cons cq;
+    struct xsk_umem *umem;
+    void *buffer;
+};
+struct xdpsock {
+    struct xsk_ring_cons rx;
+    struct xsk_ring_prod tx;
+    struct xsk_umem_info umem;
+    struct xsk_socket *xsk;
+    int fd;
+};
+static struct xdpsock xdp_socket;
+#endif
 
 /* statistics */
 struct stats {
     long long packets_received;
     long long min;
     long long max;
+    double avg;
 };
 static struct stats current_stats = {
     .packets_received = 0,
     .min = LLONG_MAX,
     .max = LLONG_MIN,
+    .avg = 0.0,
 };
 
-static int close_udp_socket(void)
+static void close_udp_socket(void)
 {
     close(udp_socket);
-    return 0;
 }
 
 static int open_udp_socket(void)
@@ -102,9 +167,34 @@ static int open_udp_socket(void)
     return 0;
 }
 
-static void *receiver_thread(void *data)
+static long long update_stats(const struct timespec *now, long long expected)
 {
-    while (23) {
+    long long diff;
+
+    /* Update stats */
+    diff = (now->tv_sec * NSEC_PER_SEC + now->tv_nsec) - expected;
+    current_stats.packets_received++;
+    current_stats.avg += diff;
+    if (diff < current_stats.min)
+        current_stats.min = diff;
+    if (diff > current_stats.max)
+        current_stats.max = diff;
+
+    return diff;
+}
+
+static void stop_tracing(void)
+{
+    int ret;
+
+    ret = system("echo 0 > /sys/kernel/debug/tracing/tracing_on");
+    if (ret)
+        log_err_errno("system() failed");
+}
+
+static void *udp_receiver_thread(void *data)
+{
+    while (!stop) {
         char buffer[1024];
         struct sockaddr_in addr;
         socklen_t addr_len;
@@ -137,13 +227,156 @@ static void *receiver_thread(void *data)
         }
 
         /* Update stats */
-        diff = (ts.tv_sec * NSEC_PER_SEC + ts.tv_nsec) - tx_time;
-        current_stats.packets_received++;
-        if (diff < current_stats.min)
-            current_stats.min = diff;
-        if (diff > current_stats.max)
-            current_stats.max = diff;
+        diff = update_stats(&ts, tx_time);
+        if (break_value_ns && diff > break_value_ns) {
+            stop_tracing();
+            stop = 1;
+            break;
+        }
     }
+
+    return NULL;
+}
+
+#ifdef WITH_XDP
+static char *parse_raw_packet(uint64_t addr, size_t len)
+{
+    char *packet, *payload;
+    struct ethhdr *eth;
+    struct iphdr *ip;
+    struct udphdr *udp;
+    size_t min_len = sizeof(*eth) + sizeof(*ip) + sizeof(*udp);
+
+    if (len <= min_len)
+        return NULL;
+
+    packet = xsk_umem__get_data(xdp_socket.umem.buffer, addr);
+
+    eth = (struct ethhdr *)packet;
+    ip  = (struct iphdr *)(packet + sizeof(*eth));
+    udp = (struct udphdr *)(packet + sizeof(*eth) + sizeof(*ip));
+
+    if (ntohs(eth->h_proto) != ETH_P_IP ||
+        ntohs(udp->dest) != atoi(port))
+        return NULL;
+
+    payload = packet + sizeof(*eth) + sizeof(*ip) + sizeof(*udp);
+
+    return payload;
+}
+#endif
+
+static void *xdp_receiver_thread(void *data)
+{
+#ifdef WITH_XDP
+    struct timespec wakeup_time;
+    struct pollfd fds[1] = { 0 };
+
+    /* poll() !? */
+    fds[0].fd     = xsk_socket__fd(xdp_socket.xsk);
+    fds[0].events = POLLIN;
+
+    /* ...or better off sleeping? */
+    if (base_time_ns)
+        ns_to_ts(base_time_ns - wakeup_time_ns, &wakeup_time);
+
+    while (!stop) {
+        char *buffer;
+        long long tx_time, diff;
+        struct timespec ts;
+        unsigned int received;
+        uint64_t addr;
+        uint32_t idx_rx = 0, len, idx;
+        int ret;
+
+        /* So, we have to options here:
+         *  1. use poll(), but that's slow :/
+         *  2. Do a mixture between active and passing waiting...
+         */
+        if (!base_time_ns) {
+            ret = poll(fds, 1, -1);
+            if (ret == 0)
+                continue;
+            if (ret < 0)
+                err_errno("poll() failed");
+
+            /*
+             * Process packets: One at a time is enough for cyclic apps.  Just
+             * be sure, that the NIC queue is correctly confgured. ethtool can
+             * be used for that.
+             */
+            received = xsk_ring_cons__peek(&xdp_socket.rx, 1, &idx_rx);
+            if (!received)
+                continue;
+        } else {
+            do {
+                ret = clock_nanosleep(CLOCK_TAI, TIMER_ABSTIME, &wakeup_time,
+                                      NULL);
+            } while (ret && errno == EINTR);
+
+            /* Busy busy... */
+            while (23) {
+                received = xsk_ring_cons__peek(&xdp_socket.rx, 1, &idx_rx);
+                if (received)
+                    break;
+            }
+
+            increment_period(&wakeup_time, intervall_ns);
+        }
+
+        /* Get current time */
+        ret = clock_gettime(CLOCK_TAI, &ts);
+        if (ret) {
+            log_err_errno("clock_gettime() failed");
+            return NULL;
+        }
+
+        if (received != 1) {
+            log_err("Received more packets than expected!");
+            continue;
+        }
+
+        /* Get the packet */
+        addr = xsk_ring_cons__rx_desc(&xdp_socket.rx, idx_rx)->addr;
+        len  = xsk_ring_cons__rx_desc(&xdp_socket.rx, idx_rx)->len;
+
+        /* Parse it */
+        buffer = parse_raw_packet(xsk_umem__add_offset_to_addr(addr), len);
+        if (!buffer)
+            continue;
+
+        /* Decode */
+        ret = sscanf(buffer, "KURT: %lld", &tx_time);
+        if (ret != 1) {
+            log_err("Failed to decode package");
+            continue;
+        }
+
+        /* Cleanup */
+        xsk_ring_cons__release(&xdp_socket.rx, received);
+
+        /* Add that particular buffer back to the fill queue */
+        if (xsk_prod_nb_free(&xdp_socket.umem.fq, received)) {
+            ret = xsk_ring_prod__reserve(&xdp_socket.umem.fq, received, &idx);
+
+            if (ret != received)
+                err("xsk_ring_prod__reserve() failed");
+
+            *xsk_ring_prod__fill_addr(&xdp_socket.umem.fq, idx) =
+                xsk_umem__extract_addr(addr);
+
+            xsk_ring_prod__submit(&xdp_socket.umem.fq, received);
+        }
+
+        /* Update stats */
+        diff = update_stats(&ts, tx_time);
+        if (break_value_ns && diff > break_value_ns) {
+            stop_tracing();
+            stop = 1;
+            break;
+        }
+    }
+#endif
 
     return NULL;
 }
@@ -158,7 +391,7 @@ static void *printer_thread(void *data)
         return NULL;
     }
 
-    while (23) {
+    while (!stop) {
         int ret;
 
         /* Sleep until next period */
@@ -173,9 +406,10 @@ static void *printer_thread(void *data)
         }
 
         /* Print stats */
-        printf("Packets: %20lld Min: %20lld [ns] Max: %20lld [ns]\r",
+        printf("Packets: %20lld Min: %20lld [ns] Max: %20lld [ns] AVG: %20lf\r",
                current_stats.packets_received, current_stats.min,
-               current_stats.max);
+               current_stats.max,
+               current_stats.avg / (double)current_stats.packets_received);
         fflush(stdout);
     }
 
@@ -184,31 +418,180 @@ static void *printer_thread(void *data)
 
 static void set_default_parameter(void)
 {
-    host     = "localhost";
-    port     = "6666";
-    priority = 99;
-    cpu      = 0;
+    host           = "localhost";
+    port           = "6666";
+    interface      = "eth0";
+    priority       = 99;
+    cpu            = 0;
+    xdp            = 0;
+    skb_mode       = 0;
+    queue          = 0;
+    break_value_ns = 0;
+    base_time_ns   = 0;
+    wakeup_time_ns = 500000;
+    intervall_ns   = 0;
 }
 
 static void print_parameter(void)
 {
     printf("------------------------------------------\n");
-    printf("Host:     %s\n", host);
-    printf("Port:     %s\n", port);
-    printf("Priority: %d\n", priority);
-    printf("CPU:      %d\n", cpu);
+    printf("Host:        %s\n", host);
+    printf("Port:        %s\n", port);
+    printf("Interface:   %s\n", interface);
+    printf("Priority:    %d\n", priority);
+    printf("CPU:         %d\n", cpu);
+    printf("XDP:         %s\n", xdp ? "Yes" : "No");
+    printf("SKB Mode:    %s\n", skb_mode ? "Yes" : "No");
+    printf("Queue:       %d\n", queue);
+    printf("Break:       %lld [ns]\n", break_value_ns);
+    printf("Base Time:   %lld [ns]\n", base_time_ns);
+    printf("Wakeup Time: %lld [ns]\n", wakeup_time_ns);
+    printf("Intervall:   %lld [ns]\n", intervall_ns);
     printf("------------------------------------------\n");
 }
 
 static void print_usage_and_die(void)
 {
     fprintf(stderr, "usage: sub [options]\n");
-    fprintf(stderr, "  -H,--host:      Remote host\n");
-    fprintf(stderr, "  -P,--port:      Remote port\n");
-    fprintf(stderr, "  -p,--priority:  Thread priority\n");
-    fprintf(stderr, "  -c,--cpu:       CPU to run on\n");
+    fprintf(stderr, "  -H,--host:        Remote host\n");
+    fprintf(stderr, "  -P,--port:        Remote port\n");
+    fprintf(stderr, "  -i,--interface:   Remote interface\n");
+    fprintf(stderr, "  -p,--priority:    Thread priority\n");
+    fprintf(stderr, "  -c,--cpu:         CPU to run on\n");
+    fprintf(stderr, "  -x,--xdp:         Use XDP instead of UDP sockets\n");
+    fprintf(stderr, "  -s,--skb_mode:    Use SKB instead of driver mode for XDP\n");
+    fprintf(stderr, "  -q,--queue:       Queue to be used for XDP\n");
+    fprintf(stderr, "  -B,--break_value: Max difference where to stop tracing\n");
+    fprintf(stderr, "  -b,--base_time:   Start time for ETF publisher\n");
+    fprintf(stderr, "  -w,--wakeup_time: Time to start polling before base time\n");
+    fprintf(stderr, "  -I,--intervall:   Period of ETF publisher\n");
 
     exit(EXIT_SUCCESS);
+}
+
+#ifdef WITH_XDP
+static inline unsigned int xdp_flags(void)
+{
+    return skb_mode ? XDP_FLAGS_SKB_MODE : XDP_FLAGS_DRV_MODE;
+}
+
+static void open_xdp_socket(void)
+{
+    struct xsk_socket_config xsk_cfg;
+    uint32_t idx;
+    int ret, i;
+
+    /* Create XDP socket */
+    xsk_cfg.rx_size      = XSK_RING_CONS__DEFAULT_NUM_DESCS;
+    xsk_cfg.tx_size      = XSK_RING_PROD__DEFAULT_NUM_DESCS;
+    xsk_cfg.libbpf_flags = 0;
+    xsk_cfg.xdp_flags    = xdp_flags();
+    xsk_cfg.bind_flags   = 0;
+
+    ret = xsk_socket__create(&xdp_socket.xsk, interface,
+                             queue, xdp_socket.umem.umem, &xdp_socket.rx,
+                             &xdp_socket.tx, &xsk_cfg);
+    if (ret)
+        err("xsk_socket__create() failed");
+
+    /* Add some buffers */
+    ret = xsk_ring_prod__reserve(&xdp_socket.umem.fq,
+                                 XSK_RING_PROD__DEFAULT_NUM_DESCS,
+                                 &idx);
+
+    if (ret != XSK_RING_PROD__DEFAULT_NUM_DESCS)
+        err("xsk_ring_prod__reserve() failed");
+
+    for (i = 0; i < XSK_RING_PROD__DEFAULT_NUM_DESCS; i++)
+        *xsk_ring_prod__fill_addr(&xdp_socket.umem.fq, idx++) =
+            i * FRAME_SIZE;
+
+    xsk_ring_prod__submit(&xdp_socket.umem.fq,
+                          XSK_RING_PROD__DEFAULT_NUM_DESCS);
+}
+#endif
+
+static void close_xdp_socket(void)
+{
+#ifdef WITH_XDP
+    xsk_socket__delete(xdp_socket.xsk);
+    xsk_umem__delete(xdp_socket.umem.umem);
+    bpf_set_link_xdp_fd(ifindex, -1, xdp_flags());
+#endif
+}
+
+static void setup_xdp_socket(void)
+{
+#ifdef WITH_XDP
+    int ret, prog_fd, xsks_map = 0;
+    struct bpf_prog_load_attr prog_load_attr = {
+        .prog_type = BPF_PROG_TYPE_XDP,
+        .file      = "xdp_kern.o",
+    };
+    struct xsk_umem_config cfg = {
+        .fill_size      = XSK_RING_PROD__DEFAULT_NUM_DESCS,
+        .comp_size      = XSK_RING_CONS__DEFAULT_NUM_DESCS,
+        .frame_size     = FRAME_SIZE,
+        .frame_headroom = XSK_UMEM__DEFAULT_FRAME_HEADROOM,
+        .flags          = 0,
+    };
+    struct bpf_object *obj;
+    struct bpf_map *map;
+    void *buffer = NULL;
+
+    ret = bpf_prog_load_xattr(&prog_load_attr, &obj, &prog_fd);
+    if (ret || prog_fd < 0)
+        err("bpf_prog_load_xattr() failed");
+
+    map = bpf_object__find_map_by_name(obj, "xsks_map");
+    xsks_map = bpf_map__fd(map);
+    if (xsks_map < 0)
+        err("No xsks_map found!");
+
+    ifindex = if_nametoindex(interface);
+    if (!ifindex)
+        err_errno("if_nametoindex() failed");
+
+    /* Use XDP _only_ in conjuction with driver assisted mode */
+    ret = bpf_set_link_xdp_fd(ifindex, prog_fd, xdp_flags());
+    if (ret)
+        err("bpf_set_link_xdp_fd() failed");
+
+    /* Allocate user space memory for xdp frames */
+    ret = posix_memalign(&buffer, sysconf(_SC_PAGE_SIZE),
+                         NUM_FRAMES * FRAME_SIZE);
+    if (ret)
+        err_errno("posix_memalign() failed");
+
+    ret = xsk_umem__create(&xdp_socket.umem.umem, buffer,
+                           NUM_FRAMES * FRAME_SIZE, &xdp_socket.umem.fq,
+                           &xdp_socket.umem.cq, &cfg);
+    if (ret)
+        err("xsk_umem__create() failed");
+    xdp_socket.umem.buffer = buffer;
+
+    /* Open and bind socket */
+    open_xdp_socket();
+#endif
+}
+
+static void sig_handler(int signal)
+{
+    stop = 1;
+}
+
+static void setup_signals(void)
+{
+    struct sigaction sa;
+
+    sigemptyset(&sa.sa_mask);
+    sa.sa_handler = sig_handler;
+    sa.sa_flags   = 0;
+
+    if (sigaction(SIGTERM, &sa, NULL))
+        err_errno("sigaction() failed");
+    if (sigaction(SIGINT, &sa, NULL))
+        err_errno("sigaction() failed");
 }
 
 int main(int argc, char *argv[])
@@ -221,7 +604,7 @@ int main(int argc, char *argv[])
 
     set_default_parameter();
 
-    while ((c = getopt_long(argc, argv, "hH:P:p:c:",
+    while ((c = getopt_long(argc, argv, "hH:P:i:p:c:xsq:B:b:w:I:",
                             long_options, NULL)) != -1) {
         switch (c) {
         case 'h':
@@ -233,18 +616,48 @@ int main(int argc, char *argv[])
         case 'P':
             port = optarg;
             break;
+        case 'i':
+            interface = optarg;
+            break;
         case 'p':
             priority = atoi(optarg);
             break;
         case 'c':
             cpu = atoi(optarg);
             break;
+        case 'x':
+            xdp = 1;
+            break;
+        case 's':
+            skb_mode = 1;
+            break;
+        case 'q':
+            queue = atoi(optarg);
+            break;
+        case 'B':
+            break_value_ns = atoll(optarg);
+            break;
+        case 'b':
+            base_time_ns = atoll(optarg);
+            break;
+        case 'w':
+            wakeup_time_ns = atoll(optarg);
+            break;
+        case 'I':
+            intervall_ns = atoll(optarg);
+            break;
         default:
             print_usage_and_die();
         }
     }
-    if (priority < 0 || cpu < 0)
+    if (priority < 0 || cpu < 0 || queue < 0 || break_value_ns < 0 ||
+        base_time_ns < 0 || wakeup_time_ns < 0)
         print_usage_and_die();
+
+#ifndef WITH_XDP
+    if (xdp)
+        err("No XDP support compiled in. Cannot use it.");
+#endif
 
     print_parameter();
 
@@ -278,17 +691,27 @@ int main(int argc, char *argv[])
     if (ret)
         pthread_err(ret, "pthread_attr_setaffinity_np() failed");
 
-    open_udp_socket();
+    if (xdp) {
+        setup_xdp_socket();
 
-    ret = pthread_create(&recv_thread, &attr, receiver_thread, NULL);
-    if (ret)
-        pthread_err(ret, "pthread_create() failed");
+        ret = pthread_create(&recv_thread, &attr, xdp_receiver_thread, NULL);
+        if (ret)
+            pthread_err(ret, "pthread_create() failed");
+    } else {
+        open_udp_socket();
+
+        ret = pthread_create(&recv_thread, &attr, udp_receiver_thread, NULL);
+        if (ret)
+            pthread_err(ret, "pthread_create() failed");
+    }
+
+    setup_signals();
 
     ret = pthread_create(&print_thread, NULL, printer_thread, NULL);
     if (ret)
         pthread_err(ret, "pthread_create() failed");
 
-    ret = pthread_setname_np(recv_thread, "EtfSubcriber");
+    ret = pthread_setname_np(recv_thread, "EtfSubscriber");
     if (ret)
         pthread_err(ret, "pthread_setname_np() failed");
 
@@ -299,7 +722,10 @@ int main(int argc, char *argv[])
     pthread_join(recv_thread, NULL);
     pthread_join(print_thread, NULL);
 
-    close_udp_socket();
+    if (xdp)
+        close_xdp_socket();
+    else
+        close_udp_socket();
 
     return EXIT_SUCCESS;
 }
---
+++ etf/xdp_kern.c
@@ -0,0 +1,62 @@
+#define KBUILD_MODNAME "blub"
+
+#include <linux/bpf.h>
+#include <linux/if_link.h>
+#include <linux/if_xdp.h>
+#include <linux/if_ether.h>
+#include <linux/udp.h>
+#include <linux/ip.h>
+#include <linux/in.h>
+
+#include "bpf_helpers.h"
+
+struct bpf_map_def SEC("maps") xsks_map = {
+    .type        = BPF_MAP_TYPE_XSKMAP,
+    .key_size    = sizeof(int),
+    .value_size  = sizeof(int),
+    .max_entries = 64,
+};
+
+static inline int parse_ipv4(void *data, unsigned long long nh_off,
+                             void *data_end)
+{
+    struct iphdr *iph = data + nh_off;
+
+    if ((void *)(iph + 1) > data_end)
+        return 0;
+
+    return iph->protocol;
+}
+
+SEC("xdp_sock")
+int xdp_sock_prog(struct xdp_md *ctx)
+{
+    void *data_end = (void *)(long)ctx->data_end;
+    void *data = (void *)(long)ctx->data;
+    struct ethhdr *eth = data;
+    int idx = ctx->rx_queue_index;
+    unsigned int ipproto = 0;
+    unsigned long long nh_off;
+
+    /* Check if it's a UDP frame: If UDP -> Redirect to active xsk for user
+     * space. If not -> pass to stack.
+     */
+    nh_off = sizeof(*eth);
+    if (data + nh_off > data_end)
+        return XDP_PASS;
+
+    if (eth->h_proto == __builtin_bswap16(ETH_P_IP))
+        ipproto = parse_ipv4(data, nh_off, data_end);
+
+    if (ipproto != IPPROTO_UDP)
+        return XDP_PASS;
+
+    /* If socket bound to rx_queue than redirect to user space */
+    if (bpf_map_lookup_elem(&xsks_map, &idx))
+        return bpf_redirect_map(&xsks_map, idx, 0);
+
+    /* Else pass to Linux' network stack */
+    return XDP_PASS;
+}
+
+char _license[] SEC("license") = "GPL";
