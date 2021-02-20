2ab36b4d48378964365f0e94c2d46c6219da7953
Kurt Kanzenbach <kurt@kmk-computers.de>
2021-02-20 13:15:06 +0100

    scripts: Replace hard coded interface
    Signed-off-by: Kurt Kanzenbach <kurt@kmk-computers.de>

--- scripts/etf.sh
+++ scripts/etf.sh
@@ -20,7 +20,7 @@ modprobe sch_mqprio
 modprobe sch_etf
 
 # Setup Tx traffic steering
-tc qdisc replace dev enp4s0 handle 100 parent root mqprio num_tc 4 \
+tc qdisc replace dev ${IF} handle 100 parent root mqprio num_tc 4 \
    map 3 3 2 1 0 3 3 3 3 3 3 3 3 3 3 3 \
    queues 1@0 1@1 1@2 1@3 \
    hw 0
@@ -32,6 +32,6 @@ tc qdisc replace dev ${IF} parent 100:1 etf \
    offload
 
 # Show end result
-tc qdisc show dev enp4s0
+tc qdisc show dev ${IF}
 
 exit 0
